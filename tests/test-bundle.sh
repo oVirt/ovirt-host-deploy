17db8cd328643ddb10fe5903c24266ae1eb698ac
Alon Bar-Lev <alonbl@redhat.com>
2012-12-02 01:30:07 +0200

    tests: use python to untar as tar of rhel-5.5 does not detect end-of-tar correctly
    Change-Id: If4fc3d870d9d9a6b3fa72258374335675cc178c2
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

--- tests/test-bundle.sh
+++ tests/test-bundle.sh
@@ -10,6 +10,7 @@ trap cleanup 0
 
 make -C "$(dirname $0)/.."  install DESTDIR="${mytmp}" &&
 	( tar -hc -C "${mytmp}"/usr/local/share/ovirt-host-deploy/interface-3 . && cat) | \
-		ssh ${HOST} '( dest=/tmp/xxx && rm -fr "${dest}" && mkdir -p "${dest}" && tar -C "${dest}" -x && \
+		ssh ${HOST} '( dest=/tmp/xxx && rm -fr "${dest}" && mkdir -p "${dest}" && \
+			python -c "import sys, tarfile; tarfile.open(fileobj=sys.stdin, mode=\"r|\").extractall(path=\"${dest}\")" && \
 			OTOPI_DEBUG=1 "${dest}"/setup "DIALOG/customization=bool:True '"$*"'" )'
 echo "exit $?"
