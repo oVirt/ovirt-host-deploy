c47209cce6834ecfa2b3f42ad4b6ad87cd149ada
ndarshan <dnarayan@redhat.com>
2014-09-04 11:43:21 +0530

    gluster: Plugin under gluster for glusterpmd configuration.
    Addition of new plugin glusterpmd(gluster process monitoring
    daemon) under gluster. This plugin starts the glusterpmd during
    host deploy if monitoring is enabled.
    
    Change-Id: I9d8f3026740be567684e13f08324a7c36be40b26
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1138198
    Signed-off-by: ndarshan <dnarayan@redhat.com>

---
+++ src/plugins/ovirt-host-deploy/gluster/glusterpmd.py
@@ -0,0 +1,59 @@
+#
+# ovirt-host-deploy -- ovirt host deployer
+# Copyright (C) 2012-2014 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+
+"""glusterpmd plugin."""
+
+
+import gettext
+_ = lambda m: gettext.dgettext(message=m, domain='ovirt-host-deploy')
+
+
+from otopi import util
+from otopi import plugin
+
+
+from ovirt_host_deploy import constants as odeploycons
+
+
+@util.export
+class Plugin(plugin.PluginBase):
+    """glusterpmd plugin"""
+
+    def __init__(self, context):
+        super(Plugin, self).__init__(context=context)
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_CLOSEUP,
+        condition=lambda self: (
+            self.environment[
+                odeploycons.GlusterEnv.MONITORING_ENABLE
+            ] and
+            self.environment[
+                odeploycons.GlusterEnv.ENABLE
+            ]
+        )
+    )
+    def _closeup(self):
+        if self.services.exists('glusterpmd'):
+            self.logger.info(_('Starting glusterpmd service'))
+            self.services.state('glusterpmd', True)
+
+
+# vim: expandtab tabstop=4 shiftwidth=4
