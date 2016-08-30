9c919f5da901580c015af6b6cac619d4cdf4fd57
Sandro Bonazzola <sbonazzo@redhat.com>
2016-08-30 13:49:13 +0200

    build: ovirt-host-deploy-1.5.2
    Change-Id: Ia5826203bd630679e8b7965e472013650faeefa5
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,7 +5,6 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 autoreconf -ivf
 ./configure
@@ -13,7 +12,6 @@ make dist
 yum-builddep ovirt-host-deploy.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
-    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-host-deploy-*.tar.gz
 
 mv *.tar.gz exported-artifacts
