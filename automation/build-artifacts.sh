8f0bf376874c1db00cbf015e17e5525d08d8d169
Yedidyah Bar David <didi@redhat.com>
2017-02-12 15:57:15 +0200

    build: post ovirt-host-deploy-1.6.0
    Change-Id: I46462c839213e34c77541fb809c0811a8e7be32a
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,6 +5,7 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 autoreconf -ivf
 ./configure
@@ -12,6 +13,7 @@ make dist
 yum-builddep ovirt-host-deploy.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-host-deploy-*.tar.gz
 
 mv *.tar.gz exported-artifacts
