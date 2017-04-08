284e1894cab1c744d9945e2b12ba46b766871462
Leon Goldberg <lgoldber@redhat.com>
2017-04-08 20:02:25 +0300

    Removing closeup stage from vdsm config plugin.
    Code was reintroduced by mistake, possibly after a bad rebase.
    
    Change-Id: I41926056987faa98f44b388e13dc731275e3dce7
    Bug-Url: https://bugzilla.redhat.com/1443064
    Signed-off-by: Leon Goldberg <lgoldber@redhat.com>
    (cherry picked from commit 175418688ca7b94605dcb15d1273fddccd7e503c)

--- src/plugins/ovirt-host-deploy/vdsm/config.py
+++ src/plugins/ovirt-host-deploy/vdsm/config.py
@@ -110,15 +110,5 @@ class Plugin(plugin.PluginBase):
                 )
             )
 
-    @plugin.event(
-        stage=plugin.Stages.STAGE_CLOSEUP,
-        condition=lambda self: self.environment[
-            odeploycons.VdsmEnv.OVIRT_VINTAGE_NODE
-        ],
-    )
-    def _closeup(self):
-        if self.services.exists('vdsm-reg'):
-            self.services.state('vdsm-reg', False)
-
 
 # vim: expandtab tabstop=4 shiftwidth=4
