87e30f30b5e879bb00bc92ced63480d551d391cb
Sandro Bonazzola <sbonazzo@redhat.com>
2017-11-17 16:35:32 +0100

    automation: test with coverage
    Change-Id: I1befdf8af4935bd5e84955ee2cc8ee7a8b635e11
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,6 +1,50 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
 autoreconf -ivf
 ./configure --enable-java-sdk
 make distcheck
 
 automation/build-artifacts.sh
+
+# install otopi from latest jenkins before restoring yum environment
+yum -y install "otopi*"
+
+#Restoring sane yum environment
+rm -f /etc/yum.conf
+yum reinstall -y system-release yum
+[[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
+[[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
+yum install -y ovirt-release-master
+rm -f /etc/yum/yum.conf
+
+yum repolist enabled
+
+yum install -y $(find "$PWD/exported-artifacts" -iname \*noarch\*.rpm)
+
+DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
+if [[ ! "${DISTVER}" == "fc" ]]; then
+# Fedora support is known to be broken, will be re-enabled once fixed
+
+save_logs() {
+mkdir -p exported-artifacts/logs
+cp -p /tmp/ovirt-host-deploy-* exported-artifacts/logs
+}
+
+trap save_logs EXIT
+
+rm -f /tmp/coverage.rc
+cp "${PWD}/automation/coverage.rc" /tmp/
+export COVERAGE_PROCESS_START="/tmp/coverage.rc"
+export COVERAGE_FILE=$(mktemp -p $PWD .coverage.XXXXXX)
+export OTOPI_DEBUG=1
+export OTOPI_COVERAGE=1
+
+automation/functional.expect
+
+unset COVERAGE_FILE
+coverage combine
+coverage html -d exported-artifacts/coverage_html_report
+cp automation/index.html exported-artifacts/
+
+fi
+
