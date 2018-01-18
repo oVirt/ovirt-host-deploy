98cfb7e4ad17c4f1a40403a767e44186088974ac
Sahina Bose <sabose@redhat.com>
2018-01-18 16:09:21 +0530

    gluster: Fixed typo with service check
    during deployment of gluster hosts.
    
    Change-Id: I17fb216900b2256b970a7ac90d2c949f317bc1ee
    Bug-Url: https://bugzilla.redhat.com/1533390
    Related-To: https://bugzilla.redhat.com/1379309
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- src/plugins/ovirt-host-deploy/gluster/packages.py
+++ src/plugins/ovirt-host-deploy/gluster/packages.py
@@ -92,7 +92,7 @@ class Plugin(plugin.PluginBase):
         self.logger.info(_('Starting gluster'))
         for state in (False, True):
             self.services.state('glusterd', state)
-        if self.services.exist('glustereventsd'):
+        if self.services.exists('glustereventsd'):
             self.logger.info(_('Starting glustereventsd'))
             self.services.startup('glustereventsd', True)
             self.services.state('glustereventsd', True)
