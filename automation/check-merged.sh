c4bd23a6c6840576097a2e7fa869e71abfa86571
Sandro Bonazzola <sbonazzo@redhat.com>
2018-02-19 15:16:55 +0100

    automation: workaround bad caching on slaves
    slaves are caching both packages and metadata causing
    mock to try installing packages which are not in some
    repos anymore.
    Cleaning the metadata before installing packages coming from
    jenkins lastSuccesfullBuild
    
    Change-Id: I4104ccc0ebb8f46fee1e3784f1d9d72615bb1d7d
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
+# workaround for bad caching on slaves
+yum --disablerepo=* --enablerepo=otopi-master-last-build clean metadata
+yum -y install otopi-devtools
+
 autoreconf -ivf
 ./configure --enable-java-sdk
 make distcheck
--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
+# workaround for bad caching on slaves
+yum --disablerepo=* --enablerepo=otopi-master-last-build clean metadata
+yum -y install otopi-devtools
+
 autoreconf -ivf
 ./configure --enable-java-sdk
 make distcheck
