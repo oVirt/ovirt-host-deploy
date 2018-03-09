34418216d2aa3b1aa238081bd08e25505fd19e6b
Sandro Bonazzola <sbonazzo@redhat.com>
2018-03-09 10:15:53 +0100

    build: post ovirt-host-deploy-1.7.3
    Change-Id: I63d91ee6d035c504e15dc2ebe466e0812e5356d5
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,6 +5,7 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
 PACKAGER=""
@@ -24,6 +25,7 @@ make dist
 yum-builddep ovirt-host-deploy.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-host-deploy-*.tar.gz
 
 mv *.tar.gz exported-artifacts
