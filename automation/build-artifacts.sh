16b7e7c0b65dac07e1f96e23d9fa5c042b878b32
Yedidyah Bar David <didi@redhat.com>
2019-01-08 10:09:35 +0200

    build: post ovirt-host-deploy-1.8.0
    Change-Id: I641a74c100c1bd2ca2718d745ccf09d341084283
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -17,12 +17,15 @@ fi
 ${PACKAGER} --disablerepo=* --enablerepo=otopi-master-last-build clean metadata
 ${PACKAGER} -y install otopi-devtools
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
 autoreconf -ivf
 ./configure
 make dist
 yum-builddep ovirt-host-deploy.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-host-deploy-*.tar.gz
 
 mv *.tar.gz exported-artifacts
