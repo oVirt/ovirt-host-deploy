a55ff7543179e756a3401a57cd61df63d75fd0c3
Sandro Bonazzola <sbonazzo@redhat.com>
2019-05-30 11:55:24 +0200

    automation: use python3 on fedora
    aligned check-merged to use python3 on fedora.
    
    Change-Id: I0d280b760d307bb25df8f60efc73eacfa856d161
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -12,6 +12,10 @@ ${PACKAGER} --disablerepo=* --enablerepo=otopi-master-last-tested clean metadata
 ${PACKAGER} -y install python2-otopi-devtools || echo "python2-otopi-devtools not found"
 ${PACKAGER} -y install python3-otopi-devtools || echo "python3-otopi-devtools not found"
 
+if [[ $(rpm --eval "%{fedora}") -gt 29 ]] ; then
+export PYTHON=python3
+fi
+
 autoreconf -ivf
 ./configure --enable-java-sdk
 make distcheck
